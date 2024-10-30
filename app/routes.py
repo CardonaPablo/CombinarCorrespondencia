175f8f9afd6381f7978c27b35518a105b1af8701
Pablo Cardona <pablo@loroinsurance.com>
2024-10-30 15:47:25 -0600

    All html templates finished no logic

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,13 @@ from flask import current_app as app, render_template
 def index():
     print("Index route")
     return render_template('index.html')
+
+@app.route('/usuarios')
+def users():
+    print("Users route")
+    return render_template('users.html')
+
+@app.route('/documentos')
+def documents():
+    print("Documents route")
+    return render_template('documents.html')
